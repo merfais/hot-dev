c940f5c964523ea94a64cc642b56534ce65d9de7
wenqing <wenqing@yunshan.net.cn>
2018-08-24 18:03:26 +0800

    commander定义与部分解析

--- index.js
+++ index.js
@@ -1,23 +1,116 @@
 'use strict'
 
 const debug = require('debug')('hot-dev')
-const watch = require('./src/watch.js')
+const chalk = require('chalk')
+const program = require('commander')
+const fs = require('fs')
+const path = require('path')
+const pkg = require('./package.json')
+const start = require('./src/command.js')
 
-const watcher = watch(__dirname, {
-  // usePolling: true,
-  ignoreInitial: true,
-}, ['add', 'addDir', 'change', 'unlink', 'unlinkDir', 'error', 'ready'])
+function getConfigFile(filePath) {
+  if (!path.isAbsolute(filePath)) {
+    filePath = path.join(__dirname, filePath)
+  }
+  const ext = path.extname(filePath)
+  if (!ext || (ext !== 'yml' && ext !== 'yaml')) {
+    if (fs.existsSync(filePath + '.yaml')) {
+      return filePath + '.yaml'
+    }
+    if (fs.existsSync(filePath + '.yml')) {
+      return filePath + '.yaml'
+    }
+    if (fs.existsSync(filePath + '.rc')) {
+      return filePath + '.rc'
+    }
+  }
+  if (fs.existsSync(filePath)) {
+    return filePath
+  }
+  console.log(chalk.red(`configuration file: '${chalk.underline(filePath)}' not exist`))
+  return false
+}
 
-let signal
-[ 'SIGINT', 'SIGQUIT', 'SIGTERM' ].forEach(event => {
-  process.once(event, () => {
-    signal = event
-    process.exit(0)
+function getEntryFile(filePath) {
+  if (!path.isAbsolute(filePath)) {
+    filePath = path.join(__dirname, filePath)
+  }
+  const ext = path.extname(filePath)
+  if (!ext || ext !== 'js') {
+    filePath += '.js'
+  }
+  if (fs.existsSync(filePath)) {
+    return filePath
+  }
+  console.log(chalk.red(`entry file: '${chalk.underline(filePath)}' not exist`))
+  return false
+}
+
+function getWathPath(pattern, value) {
+
+}
+
+function getOptions(option, value) {
+
+}
+
+function getEvents(event, value) {
+
+}
+
+program
+  .version(pkg.version, '-v --version')
+  .usage('[Option] [value] | <Command> [Option]')
+  .option('-c --config <filePath>', 'path to the config file', getConfigFile)
+  .option('-f --file <filePath>', 'path to the entry file', getEntryFile)
+  .option('-w --watch <file | directory>', 'Paths to file or directory or glob pattern \n \
+                                  to be watched recursively\n \
+                                  use "," to split multiple pattern', getWathPath, [])
+  .option('-i --exclude <file | directory>', 'exclude file or directory or glob pattern \n \
+                                  to be watched \n \
+                                  use "," to split multiple pattern', getWathPath, [])
+  .option('-o --option <name=value>', 'set watch options \n \
+                                  use "," to split multiple pattern \n \
+                                  see https://github.com/paulmillr/chokidar#api', getOptions, {})
+  .option('-e --event <event>', 'which event will be listened, Available events:\n \
+                                  add, addDir, change, unlink, unlinkDir, ready, raw, error \n \
+                                  use "," to split multiple pattern \n \
+                                  see https://github.com/paulmillr/chokidar#methods--events', getEvents, [])
+
+program
+  .command('init')
+  .description('create a hot-dev configuration file')
+  .option('-d --default', 'skip prompts and use default preset')
+  .action(cmd => {
+    // TODO:
   })
-})
 
-process.once('exit', () => {
-  debug('kill hot-dev:%s with %s', watcher.pid, signal);
-  watcher.close()
-  // watcher.kill(signal)
-})
+program.parse(process.argv)
+
+debug('argv.config = %s', program.config)
+debug('argv.file = %s', program.file)
+
+if (program.config !== false && program.file !== false) {
+  const watcher = start(
+    program.config,
+    program.file,
+    program.watch,
+    program.exclude,
+    program.option,
+    program.event,
+  )
+
+  const signals = ['SIGINT', 'SIGQUIT', 'SIGTERM']
+  signals.forEach(signal => {
+    process.once(signal, () => {
+      process.exit(signal)
+    })
+  })
+
+  process.once('exit', (signal) => {
+    console.log()
+    debug('kill hot-dev:%s with %s', watcher.pid, signal);
+    watcher.close()
+    // watcher.kill(signal)
+  })
+}
--- src/command.js
+++ src/command.js
@@ -1,12 +1,192 @@
 'use strict'
 
 const debug = require('debug')('hot-dev')
-const chalk = require('chalk')
-const commander = require('commander')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+const yaml = require('js-yaml')
 
-const pkg = require('../package.json')
+const watch = require('./watch.js')
 
-commander
-  .version(pkg.version, '-v --version')
-  .option('-c --config <filePath>', 'path to the config file', getConfigFile)
-  .option('-o --option [name]=[value]')
+function defaultConfig() {
+  return {
+    paths: [__dirname],
+    options: {
+      persistent: true,
+      ignored: '',
+      ignoreInitial: true,
+      followSymlinks: true,
+      cwd: '',
+      disableGlobbing: false,
+      usePolling: true,
+      interval: 100,
+      binaryInterval:300,
+      useFsEvents: true,
+      alwaysStat: true,
+      depth: 99,
+      awaitWriteFinish: {
+        stabilityThreshold: 2000,
+        pollInterval: 100,
+      },
+      ignorePermissionErrors: false,
+      atomic: 100,
+    },
+    events: [
+      'change',
+    ]
+  }
+}
+
+function readFrom(file) {
+  debug('try read file: %s' file)
+  let content = false
+  try {
+    content = yaml.safeLoad(fs.readFileSync(file, 'utf8'))
+  } catch (e) {
+    debug('read file: %s field', file)
+  }
+  return content
+}
+
+function getConfig(path) {
+  let content = false
+  if (path === undefined) {
+    const tryFiles = [
+      path.resolve('.hot-dev.yaml'),
+      path.resolve('hot-dev.yaml'),
+      path.resolve('.hot-dev.yml'),
+      path.resolve('hot-dev.yml'),
+      path.resolve('.hot-dev.rc'),
+      path.resolve('hot-dev.rc'),
+      path.resolve(os.homedir(), '.hot-dev.yaml'),
+      path.resolve(os.homedir(), 'hot-dev.yaml'),
+      path.resolve(os.homedir(), '.hot-dev.yml'),
+      path.resolve(os.homedir(), 'hot-dev.yml'),
+      path.resolve(os.homedir(), '.hot-dev.rc'),
+      path.resolve(os.homedir(), 'hot-dev.rc'),
+    ]
+    let i = 0
+    while (i < tryFiles.length) {
+      content = readFrom(tryFiles[i])
+      if (content) {
+        break;
+      }
+      i += 1
+    }
+  } else {
+    content = readFrom(path)
+  }
+  if (!content) {
+    content = defaultConfig()
+  }
+  return content
+}
+
+function getEntry(path) {
+  if (path === undefined) {
+    const tryFiles = [
+      path.resolve('index.js')
+      path.resolve('Index.js')
+      path.resolve('app.js')
+      path.resolve('App.js')
+      path.resolve('start.js')
+      path.resolve('Start.js')
+      path.resolve('master.js')
+      path.resolve('Master.js')
+    ]
+    let i = 0
+    while (i < tryFiles.length) {
+      if (fs.existsSync(tryFiles[i])) {
+        path = tryFiles[i]
+        break
+      }
+      i += 1
+    }
+    if (!path) {
+      console.log(chalk.red(`can't find entry file in ${chalk.underline(__dirname)}`))
+      console.log(chalk.greenBright(
+        '    please use -f <filePath> to specify a entry file' +
+        'or touch a file with following names in this directory: \n' +
+        '    index.js\n' +
+        '    Index.js\n' +
+        '    app.js\n' +
+        '    App.js\n' +
+        '    start.js\n' +
+        '    Start.js\n' +
+        '    master.js\n' +
+        '    Master.js\n'
+      ))
+      return false
+    }
+  } else {
+    if (!path.isAbsolute(path)) {
+      path = path.resolve(path)
+    }
+    if (!fs.existsSync(path)) {
+      const dir = path.dirname(path)
+      const baseName = path.basename(path)
+      console.log(chalk.red(
+        `can't find entry file(${chalk.underline(baseName)})` +
+        ` in ${chalk.underline(dir)}, ` +
+        'please specify another file'
+      ))
+      return false
+    }
+  }
+  return path
+}
+
+function genOptions(content, watches, excludes, exOptions, events) {
+  const options = content && content.options || {}
+  if (Object.prototype.toString.call(exOptions) === '[object Object]') {
+    const awaitWriteFinish = exOptions.awaitWriteFinish
+    const stabilityThreshold = exOptions.stabilityThreshold
+    const pollInterval = exOptions.pollInterval
+    delete exOptions.awaitWriteFinish
+    delete exOptions.stabilityThreshold
+    delete exOptions.pollInterval
+    const keys = Object.keys(exOptions)
+    let i = 0
+    while (i < keys.length) {
+      options[keys[i]] = exOptions[keys[i]]
+      i += 1
+    }
+    if (awaitWriteFinish === false) {
+      options.awaitWriteFinish = false
+    } else if (awaitWriteFinish === true) {
+      options.awaitWriteFinish = true
+    } else {
+      options.awaitWriteFinish = {}
+      if (stabilityThreshold !== undefined) {
+        options.awaitWriteFinish.stabilityThreshold = stabilityThreshold
+      }
+      if (pollInterval !== undefined) {
+        options.awaitWriteFinish.pollInterval = pollInterval
+      }
+    }
+  }
+  if (Array.isArray(excludes) && excludes.length > 0) {
+    options.ignored = excludes
+  }
+  content.options = options
+  if (Array.isArray(watches) && watches.length > 0) {
+    content.paths = watches
+  }
+  if (Array.isArray(events) && events.length > 0) {
+    content.events = events
+  }
+  return content
+}
+
+function start(configPath, entryPath, watches, excludes, exOptions, events) {
+  const content = readFrom(configPath)
+  const options = genOptions(content, watches, excludes, exOptions, events)
+  const entry = getEntry(entryPath)
+  if (entry) {
+
+  }
+  const watcher = watch(options)
+  return watcher
+}
+
+exports = module.exports = start
